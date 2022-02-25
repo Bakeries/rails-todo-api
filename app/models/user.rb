31bad4577eec5728b1260570c29d79144d9b4c9b
Constantinos Constantinides <bakeries.g9@gmail.com>
2022-02-25 16:29:52 +0200

    making user unique

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,11 @@ class User < ApplicationRecord
   has_secure_password
 
   # Model associations
-  has_many :todos, foreign_key: :created_by
+  has_many :todos, :dependent => :destroy, foreign_key: :created_by
   # Validations
   validates_presence_of :username, :password_digest
+
+  # Making each user unique
+  validates :username, presence: true
+  validates :username, uniqueness: {message: "Username has already been taken"}
 end
