09298c7c8ae60f1ec8f8659669c9fa26e761328b
Constantinos Constantinides <bakeries.g9@gmail.com>
2022-02-25 18:05:07 +0200

    fixing bug of logout

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  skip_before_action :authorize_request, only: :create, only: :logout
+  skip_before_action :authorize_request, only: [:create, :logout]
   # POST /signup
   # return authenticated token upon signup
   def create
